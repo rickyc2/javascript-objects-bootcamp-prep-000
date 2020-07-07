4ef7566b821dad6161154638f4e777d7edf9e49b
darling-thread-6760 <mistawilliams210@gmail.com>
2020-07-07 23:13:37 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,7 +2,7 @@ var playlist = { queen:'eterno', alanis:'thank U'};
 
  function updatePlaylist(playlist,artistName,songTitle){
  playlist[artistName] = songTitle;
- return updatePlaylist
+ return playlist;
 }
 function removeFromPlaylist(playlist,artistName){
 delete playlist.queen;
